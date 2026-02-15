b8a47b01a732442a51cba62e2d2ef5bff89401ee
PraptiH <praptiantara1892@gmail.com>
2026-02-15 10:45:34 +0600

    Order numbers sequentially using sort function

---
+++ problem2.js
@@ -0,0 +1,3 @@
+const rollNumbers = [5, 2, 16, 4, 17, 1, 20, 8, 10, 19, 14, 6, 18, 9, 13, 3, 15, 7, 11, 12];
+const sortedRollNumbers = rollNumbers.sort(function (a, b) { return a - b })
+console.log(sortedRollNumbers)
