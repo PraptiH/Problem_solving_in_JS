a87278d065a1bfe3e54c6e819a656054fb8c7b6b
PraptiH <praptiantara1892@gmail.com>
2026-02-15 12:42:16 +0600

    Modify the the leap year condition

--- problem6.js
+++ problem6.js
@@ -1,8 +1,9 @@
 const year = [2002, 2004, 2008, 2012, 1002, 2024]
 const leapYear = []
 for (let i = 0; i < year.length; i++) {
-    if (year[i] % 4 == 0 && year[i] % 100 != 0) {
+    if (year[i] % 400 === 0 || (year[i]%4 ===0 && year[i] % 100 !== 0)) {
         leapYear.push(year[i])
-        console.log(leapYear)
     }
 }
+console.log(leapYear)
+     
