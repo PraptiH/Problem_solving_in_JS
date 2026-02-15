b776ac2ff7406f1ae0d010a14bb775122963bf52
PraptiH <praptiantara1892@gmail.com>
2026-02-15 11:30:10 +0600

    Extract unique number from an array using set

--- problem4.js
+++ problem4.js
@@ -1,11 +1,4 @@
 const num = [3,8,5,7,9,3,9,5,];
-const uniqueNum = [];
-for(let i=0; i<num.length;i++){
-    const currentNum = num[i];
-    if(!uniqueNum.includes(currentNum)){
-        uniqueNum.push(currentNum)
-    }
-  
-}
- console.log(uniqueNum)
+const uniqueNum = [...new Set(num)]
+console.log(uniqueNum)
 
