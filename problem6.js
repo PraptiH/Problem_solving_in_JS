fc486af15e3b3ec7c4ceefd1cf402ca046877e7d
PraptiH <praptiantara1892@gmail.com>
2026-02-15 12:33:55 +0600

    Find leap year from an array

---
+++ problem6.js
@@ -0,0 +1,8 @@
+const year = [2002, 2004, 2008, 2012, 1002, 2024]
+const leapYear = []
+for (let i = 0; i < year.length; i++) {
+    if (year[i] % 4 == 0 && year[i] % 100 != 0) {
+        leapYear.push(year[i])
+        console.log(leapYear)
+    }
+}
