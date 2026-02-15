44ab14493dbd253cfd13f37ad0e1d0e586be3abf
PraptiH <praptiantara1892@gmail.com>
2026-02-15 10:46:36 +0600

    Order numbers sequentially using loop

--- problem2.js
+++ problem2.js
@@ -1,3 +1,12 @@
 const rollNumbers = [5, 2, 16, 4, 17, 1, 20, 8, 10, 19, 14, 6, 18, 9, 13, 3, 15, 7, 11, 12];
 const sortedRollNumbers = rollNumbers.sort(function (a, b) { return a - b })
-console.log(sortedRollNumbers)
+
+
+for (let i = 0; i < rollNumbers.length; i++) {
+    if (rollNumbers[i] > rollNumbers[i] + 1) {
+        console.log(rollNumbers[i] + 1)
+    }
+    else {
+        console.log(rollNumbers[i])
+    }
+}
