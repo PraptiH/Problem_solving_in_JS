36a70f169cc88ef859d0312c80152fa789ba74b5
PraptiH <praptiantara1892@gmail.com>
2026-02-16 16:07:46 +0600

    Find the first position of a word from a sentence

--- problem8.js
+++ problem8.js
@@ -1,4 +1,10 @@
 const sen = "Learn with sumit is all about teaching web development skills and techniques in an efficient and practical manner. If you are just getting started in web development, Learn with Sumit has all the tools you need to learn the newest and most popular technologies to convert you from a no stack to full stack developer. Learn with Sumit also deep dives into advanced topics using the latest best practices for you seasonded web developer"
- const matched = sen.match(/learn/gi);
- const appear = matched ? matched.length : 0;
- console.log(appear)
+const matched = sen.match(/learn/gi);
+console.log(matched)
+
+const appear = matched ? matched.length : 0;
+console.log(appear)
+
+let position = sen.search(/developer/i);
+position = position > 0 ? position : "Not Found"
+console.log(position)
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,2 +1,7 @@
-const uniqueNum = [...new Set(num)]
-console.log(uniqueNum)
+const sen = "Learn with sumit is all about teaching web development skills and techniques in an efficient and practical manner. If you are just getting started in web development, Learn with Sumit has all the tools you need to learn the newest and most popular technologies to convert you from a no stack to full stack developer. Learn with Sumit also deep dives into advanced topics using the latest best practices for you seasonded web developer"
+ const matched = sen.match(/learn/gi);
+ console.log(matched)
+ const appear = matched ? matched.length : 0;
+ console.log(appear)
+ const position = sen.search(/developer/i);
+ console.log(position)
